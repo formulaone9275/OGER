b7cca7edf596ca5ce430f3e9ff39e1406d2f705b
formulaone9275 <psu@udel.edu>
2021-01-19 18:10:43 -0500

    edit

--- complex_extraction.py
+++ complex_extraction.py
@@ -22,11 +22,10 @@ chain_pattern_2='(\d+\s?kDa)?\s?chain'
 kDa_pattern='\d+\s?kDa'
 
 #for the information after subunit
-after_subunit_alpha='[Aa]lpha'
-after_subunit_beta='[Bb]eta'
-after_subunit_gamma='[Gg]amma'
-after_subunit_delta='[Dd]elta'
-after_subunit_sigma='[Ss]igma'
+greek_list=['[Aa]lpha','[Bb]eta','[Gg]amma','[Dd]elta','[Ss]igma']
+greek_list_short=[gi[1] for gi in greek_list]
+after_subunit_greek='('+'|'.join(greek_list)+')'
+after_subunit_greek_digit='('+'|'.join(greek_list)+')-?\s?(\d+)([A-Z]?)'
 
 after_subunit_digit='\d+'
 after_subunit_cap_letter='[A-Z]{1,3}'
@@ -40,9 +39,8 @@ complie_chain_pattern_2=re.compile(chain_pattern_2)
 
 complie_kDa_pattern=re.compile(kDa_pattern)
 
-complie_after_subunit_alpha=re.compile(after_subunit_alpha)
-complie_after_subunit_beta=re.compile(after_subunit_beta)
-
+complie_after_subunit_greek=re.compile(after_subunit_greek)
+complie_after_subunit_greek_digit=re.compile(after_subunit_greek_digit)
 
 complie_after_subunit_single_digit=re.compile(after_subunit_digit)
 complie_after_subunit_single_letter=re.compile(after_subunit_cap_letter)
@@ -54,12 +52,15 @@ for ki in uniprot_kb_protein_dic.keys():
 
         sr_subunit_1=complie_subunit_pattern_1.search(protein_name)
         sr_subunit_2=complie_subunit_pattern_2.search(protein_name)
+
+        sr_chain_1=complie_chain_pattern_1.search(protein_name)
+        sr_chain_2=complie_chain_pattern_2.search(protein_name)
         sr_kda=complie_kDa_pattern.search(protein_name)
 
         #find the kda digit to remove relevant the gene name
         #[True/False, kDa digit]
         protein_kda_dic[ki]=[False,'']
-        #[alpha, beta, digit, single letter]
+        #[greek,greek+digit , digit, single letter]
         protein_subunit_info_dic[ki]=['','','','']
         if sr_kda:
 
@@ -67,54 +68,74 @@ for ki in uniprot_kb_protein_dic.keys():
             kda_digit=kda_digit.strip()
             protein_kda_dic[ki]=[True,kda_digit]
 
-        if sr_subunit_1:
+        if sr_subunit_1 or sr_chain_1:
+            if sr_subunit_1 and sr_chain_1:
+                if sr_subunit_1.span()[1]>sr_chain_1.span()[1]:
+                    sr_1=sr_chain_1
+                else:
+                    sr_1=sr_subunit_1
+            elif sr_subunit_1:
+                sr_1=sr_subunit_1
+            elif sr_chain_1:
+                sr_1=sr_chain_1
+
             if ki in complex_dic:
-                complex_dic[ki].append([ki,protein_name,protein_name[:sr_subunit_1.span()[0]]])
+                complex_dic[ki].append([ki,protein_name,protein_name[:sr_1.span()[0]]])
             else:
-                complex_dic[ki]=[[ki,protein_name,protein_name[:sr_subunit_1.span()[0]]]]
+                complex_dic[ki]=[[ki,protein_name,protein_name[:sr_1.span()[0]]]]
             #print('ID:',ki)
             #print('Original protein name: ',protein_name)
             #print(sr_subunit_1.span())
             #print('Complex name: ',protein_name[:sr_subunit_1.span()[0]])
-            after_subunit_str=protein_name[sr_subunit_1.span()[1]+1:]
+            after_subunit_str=protein_name[sr_1.span()[1]+1:]
             after_subunit_str=after_subunit_str.strip()
-            sr_alpha=complie_after_subunit_alpha.search(after_subunit_str)
-            sr_beta=complie_after_subunit_beta.search(after_subunit_str)
-            if sr_alpha:
-                protein_subunit_info_dic[ki][0]='alpha'
-            if sr_beta:
-                protein_subunit_info_dic[ki][1]='beta'
+            sr_greek=complie_after_subunit_greek.search(after_subunit_str)
+            sr_greek_digit=complie_after_subunit_greek_digit.search(after_subunit_str)
+            if sr_greek:
+                protein_subunit_info_dic[ki][0]=sr_greek.group(0)
+            if sr_greek_digit:
+                protein_subunit_info_dic[ki][1]=sr_greek_digit.group(1)[0].upper()+sr_greek_digit.group(2)
             if after_subunit_str.isdigit():
                 protein_subunit_info_dic[ki][2]=after_subunit_str
             if len(after_subunit_str)==1:
                 protein_subunit_info_dic[ki][3]=after_subunit_str
 
-        elif sr_subunit_2:
+        elif sr_subunit_2 or sr_chain_2:
+            if sr_subunit_2 and sr_chain_2:
+                if sr_subunit_2.span()[1]>sr_chain_2.span()[1]:
+                    sr_2=sr_chain_2
+                else:
+                    sr_2=sr_subunit_2
+            elif sr_subunit_2:
+                sr_2=sr_subunit_2
+            elif sr_chain_2:
+                sr_2=sr_chain_2
+
             if ki in complex_dic:
-                complex_dic[ki].append([ki,protein_name,protein_name[:sr_subunit_2.span()[0]]])
+                complex_dic[ki].append([ki,protein_name,protein_name[:sr_2.span()[0]]])
             else:
-                complex_dic[ki]=[[ki,protein_name,protein_name[:sr_subunit_2.span()[0]]]]
+                complex_dic[ki]=[[ki,protein_name,protein_name[:sr_2.span()[0]]]]
             #print('ID:',ki)
             #print('Original protein name: ',protein_name)
             #print(sr_subunit_2.span())
             #print('Complex name: ',protein_name[:sr_subunit_2.span()[0]])
-            after_subunit_str=protein_name[sr_subunit_2.span()[1]+1:]
+            after_subunit_str=protein_name[sr_2.span()[1]+1:]
             after_subunit_str=after_subunit_str.strip()
-            sr_alpha=complie_after_subunit_alpha.search(after_subunit_str)
-            sr_beta=complie_after_subunit_beta.search(after_subunit_str)
-            if sr_alpha:
-                protein_subunit_info_dic[ki][0]='alpha'
-            if sr_beta:
-                protein_subunit_info_dic[ki][1]='beta'
+            sr_greek=complie_after_subunit_greek.search(after_subunit_str)
+            sr_greek_digit=complie_after_subunit_greek_digit.search(after_subunit_str)
+            if sr_greek:
+                protein_subunit_info_dic[ki][0]=sr_greek.group(0)
+            if sr_greek_digit:
+                protein_subunit_info_dic[ki][1]=sr_greek_digit.group(1)[0].upper()+sr_greek_digit.group(2)
             if after_subunit_str.isdigit():
                 protein_subunit_info_dic[ki][2]=after_subunit_str
-            if len(after_subunit_str)==1:
+            if len(after_subunit_str)==1 and not after_subunit_str.isdigit():
                 protein_subunit_info_dic[ki][3]=after_subunit_str
 
 
 #for the information after subunit
-gene_alpha_pattern='A\d*$'
-gene_beta_pattern='B\d*$'
+gene_greek_pattern='('+'|'.join(greek_list_short)+')\d*$'
+
 gene_single_digit_pattern='\d+$'
 gene_single_letter_pattern='[A-Z]$'
 for ki in uniprot_kb_gene_dic.keys():
@@ -133,46 +154,56 @@ for ki in uniprot_kb_gene_dic.keys():
 
         if ki in protein_subunit_info_dic:
             if len(protein_subunit_info_dic[ki][0])>0:
-                complie_gene_alpha=re.compile(gene_alpha_pattern)
-                sr_gene_alpha=complie_gene_alpha.search(gene_name)
-                if sr_gene_alpha:
-                    if ki in complex_dic:
-                        complex_dic[ki].append([ki,gene_name,gene_name[:sr_gene_alpha.span()[0]]])
-                    else:
-                        complex_dic[ki]=[[ki,gene_name,gene_name[:sr_gene_alpha.span()[0]]]]
-                    print([ki,gene_name,gene_name[:sr_gene_alpha.span()[0]]])
+                complie_gene_greek=re.compile(gene_greek_pattern)
+                sr_gene_greek=complie_gene_greek.search(gene_name)
+                if sr_gene_greek:
+                    #if the matched gene part is the same with the match protein part
+                    #for example A <==> alpha
+                    if sr_gene_greek.group(0)[0].upper()==protein_subunit_info_dic[ki][0][0].upper():
+                        if ki in complex_dic:
+                            complex_dic[ki].append([ki,gene_name,gene_name[:sr_gene_greek.span()[0]]])
+                        else:
+                            complex_dic[ki]=[[ki,gene_name,gene_name[:sr_gene_greek.span()[0]]]]
+                        print('Only greek',[ki,gene_name,gene_name[:sr_gene_greek.span()[0]]])
 
-            elif len(protein_subunit_info_dic[ki][1])>0:
-                complie_gene_beta=re.compile(gene_beta_pattern)
-                sr_gene_beta=complie_gene_beta.search(gene_name)
-                if sr_gene_beta:
-                    if ki in complex_dic:
-                        complex_dic[ki].append([ki,gene_name,gene_name[:sr_gene_beta.span()[0]]])
-                    else:
-                        complex_dic[ki]=[[ki,gene_name,gene_name[:sr_gene_beta.span()[0]]]]
-                    print([ki,gene_name,gene_name[:sr_gene_beta.span()[0]]])
+            if len(protein_subunit_info_dic[ki][1])>0:
+                complie_gene_greek=re.compile(gene_greek_pattern)
+                sr_gene_greek=complie_gene_greek.search(gene_name)
+                if sr_gene_greek:
+                    #if the matched gene part is the same with the match protein part
+                    #for example A1 <==> alpha-1
+                    if sr_gene_greek.group(0).endswith(protein_subunit_info_dic[ki][1]) or \
+                        sr_gene_greek.group(0).endswith(protein_subunit_info_dic[ki][1][0]):
+                        if ki in complex_dic:
+                            complex_dic[ki].append([ki,gene_name,gene_name[:sr_gene_greek.span()[0]]])
+                        else:
+                            complex_dic[ki]=[[ki,gene_name,gene_name[:sr_gene_greek.span()[0]]]]
+                        print('Greek+digit:',[ki,gene_name,gene_name[:sr_gene_greek.span()[0]]])
 
-            elif len(protein_subunit_info_dic[ki][2])>0:
-                complie_single_digit=re.compile(protein_subunit_info_dic[ki][2]+'$')
+            if len(protein_subunit_info_dic[ki][2])>0:
+                complie_single_digit=re.compile('\D'+protein_subunit_info_dic[ki][2]+'$')
                 sr_single_digit=complie_single_digit.search(gene_name)
+                
                 if sr_single_digit:
                     if ki in complex_dic:
                         complex_dic[ki].append([ki,gene_name,gene_name[:sr_single_digit.span()[0]]])
                     else:
                         complex_dic[ki]=[[ki,gene_name,gene_name[:sr_single_digit.span()[0]]]]
-                    print([ki,gene_name,gene_name[:sr_single_digit.span()[0]]])
+                    print('Single digit',[ki,gene_name,gene_name[:sr_single_digit.span()[0]]])
 
-            elif len(protein_subunit_info_dic[ki][3])>0:
+            if len(protein_subunit_info_dic[ki][3])>0:
                 complie_gene_single_letter=re.compile(protein_subunit_info_dic[ki][3]+'$')
                 sr_gene_single_letter=complie_gene_single_letter.search(gene_name)
+                if ki=='Q7Z2R4':
+                    print(sr_gene_single_letter)
                 if sr_gene_single_letter:
                     if ki in complex_dic:
                         complex_dic[ki].append([ki,gene_name,gene_name[:sr_gene_single_letter.span()[0]]])
                     else:
                         complex_dic[ki]=[[ki,gene_name,gene_name[:sr_gene_single_letter.span()[0]]]]
-                    print([ki,gene_name,gene_name[:sr_gene_single_letter.span()[0]]])
+                    print('Single letter:',[ki,gene_name,gene_name[:sr_gene_single_letter.span()[0]]])
 
-complex_file='./Glygen/OGER/complex_dic.csv'
+complex_file='./Glygen/OGER/complex_dic_test.csv'
 with codecs.open(complex_file, 'w',encoding='utf-8') as tsvfile1:
     spamwriter = csv.writer(tsvfile1, delimiter='\t', quotechar='|')
     col_title=['cui','resource','original_id','term','preferred_term','entity_type']
