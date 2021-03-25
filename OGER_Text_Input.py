29d14f8abae40fd7cf94cf11508c0108049247bb
formulaone9275 <psu@udel.edu>
2021-03-25 14:17:48 -0400

    add

--- OGER_Text_Input.py
+++ OGER_Text_Input.py
@@ -1,14 +1,14 @@
 from oger.ctrl.router import Router, PipelineServer
 from nltk.corpus import words
-import json,csv,re
+import json,csv,re,os
 
 
-def protein_detection_OGER(term_file,txt_file):
+def protein_detection_OGER(term_file,full_length_txt_path):
     conf = Router(termlist_path=term_file)
     pl = PipelineServer(conf)
 
     english_dict=words.words()
-    word_extra=['has','Post','To']
+    word_extra=['has','Post','To','Large']
     word_delete_from_dic=['thyroglobulin','albumin','insulin','rhodopsin',\
         'thyroglobulin','cholinesterase','prothrombin','prolactin','catalase','chymase','tyrosinase',\
             'elastin','renin','agrin',]
@@ -18,56 +18,64 @@ def protein_detection_OGER(term_file,txt_file):
         if wi in english_dict:
             english_dict.remove(wi)
     amino_acid_shrot=['Ala', 'Arg', 'Asn', 'Asp', 'Cys', 'Glu', 'Gln', 'Gly', 'His', 'Hyp', 'Ile', 'Leu', 'Lys', 'Met', 'Phe', 'Pro', 'Glp', 'Ser', 'Thr', 'Trp', 'Tyr', 'Val',]
-    txt_file='./Glygen/OGER/8172892.txt'
+    
 
     entity_dic={}
-
-    coll = pl.load_one(txt_file, 'txt')
-    pl.process(coll)
-    #print(coll[0][0].text)
-    filter_out_protein_pattern='^([A-Za-z]\s?\d|[A-Za-z]{2})$'
-    com_protein_filter_out=re.compile(filter_out_protein_pattern)
-    filter_out_protein_list=['and 1','at 1','solved at 1','GP1','GP2','at 2']
-    
-    
-    entities_list=[]
-    for ei in coll[0].iter_entities():
-        #print(ei.text)
-        sr=com_protein_filter_out.search(ei.text)
-        name_is_digit=ei.text.isdigit()
+    #process txt file one by one
+    for fi in os.listdir(full_length_txt_path):
+        if not fi.endswith('txt'):
+            continue
         
-        if ei.info[2]=='Swiss-Prot' and ei.text not in english_dict and \
-                ei.text not in amino_acid_shrot and not sr and not name_is_digit and \
-                    ei.text not in filter_out_protein_list:
-            #print((ei.text, ei.info[3], ei.start, ei.end))
-            entities_list.append([ei.text,ei.info[3],int(ei.start),int(ei.end)])
-            
+        coll = pl.load_one(full_length_txt_path+fi, 'txt')
 
-    #in some cases, OGER will detect entities that are overlapped to each other
-    #like ['Tissue plasminogen activator', 'P00750', 0, 28], ['plasminogen', 'P00747', 7, 18]
+        file_id=fi.split('.')[0]
+        pl.process(coll)
+        #print(coll[0][0].text)
+        filter_out_protein_pattern='^([A-Za-z]\s?\d|[A-Za-z]{2})$'
+        com_protein_filter_out=re.compile(filter_out_protein_pattern)
+        filter_out_protein_list=['and 1','at 1','solved at 1','GP1','GP2','at 2','Fig']
     
-    entities_list_non_overlap=[]
-    for entity_i in entities_list:
-        overlap_sign=False
-        for entity_ii in entities_list:
-            if (entity_i[2]>entity_ii[2] and entity_i[3]<=entity_ii[3]) or \
-            (entity_i[2]>=entity_ii[2] and entity_i[3]<entity_ii[3]) or \
-                (entity_i[2]>entity_ii[2] and entity_i[3]<entity_ii[3]):
-                overlap_sign=True
-                break
-        if not overlap_sign:
-            entities_list_non_overlap.append(entity_i)
-    entity_dic['8172892']=entities_list_non_overlap
+        entities_list=[]
+        
+        for ei in coll[0].iter_entities():
+            #print(ei.text)
+            
+            sr=com_protein_filter_out.search(ei.text)
+            name_is_digit=ei.text.isdigit()
+            
+            if ei.info[2]=='Swiss-Prot' and ei.text not in english_dict and \
+                    ei.text not in amino_acid_shrot and not sr and not name_is_digit and \
+                        ei.text not in filter_out_protein_list:
+                #print((ei.text, ei.info[3], ei.start, ei.end))
+                entities_list.append([ei.text,ei.info[3],int(ei.start),int(ei.end)])
+                
+    
+        #in some cases, OGER will detect entities that are overlapped to each other
+        #like ['Tissue plasminogen activator', 'P00750', 0, 28], ['plasminogen', 'P00747', 7, 18]
+        
+        entities_list_non_overlap=[]
+        for entity_i in entities_list:
+            overlap_sign=False
+            for entity_ii in entities_list:
+                if (entity_i[2]>entity_ii[2] and entity_i[3]<=entity_ii[3]) or \
+                (entity_i[2]>=entity_ii[2] and entity_i[3]<entity_ii[3]) or \
+                    (entity_i[2]>entity_ii[2] and entity_i[3]<entity_ii[3]):
+                    overlap_sign=True
+                    break
+            if not overlap_sign:
+                entities_list_non_overlap.append(entity_i)
+        entity_dic[file_id]=entities_list_non_overlap
+            
     return entity_dic
-#print(entity_dic['12654314'])
+
 
 def merge_oger_detections(term_file,term_file_sorted,output_json):
 
     #use different term file to detect entities and then merge them
-    pmid_file='./Glygen/OGER/glygen_set.txt'
-    entity_dic=protein_detection_OGER(term_file,pmid_file)
+    full_length_txt_path='./Glygen/OGER/full_length_txt/'
+    entity_dic=protein_detection_OGER(term_file,full_length_txt_path)
     
-    entity_dic_sorted=protein_detection_OGER(term_file_sorted,pmid_file)
+    entity_dic_sorted=protein_detection_OGER(term_file_sorted,full_length_txt_path)
 
     #this is for logging the protein names that are normalized to more than one uniprot ids
     multiple_ids_protein_dic={}
@@ -146,28 +154,30 @@ def merge_oger_detections(term_file,term_file_sorted,output_json):
                 spamwriter_mpi.writerow(list(ri))
 
 if __name__=='__main__':
+    '''
+    term_file='./Glygen/OGER/uniprot_human_sprot_complex_final.csv'
+    term_file_sorted='./Glygen/OGER/uniprot_human_sprot_complex_sorted.csv'
+    output_json='./Glygen/OGER/glygen_large_oger_entity_complex.json'
 
-    term_file='./Glygen/OGER/uniprot_human_sprot_final.csv'
-    term_file_sorted='./Glygen/OGER/uniprot_human_sprot_sorted.csv'
-    output_json='./Glygen/OGER/oger_entity_complex.json'
-
-    print(protein_detection_OGER(term_file,''))
+    merge_oger_detections(term_file,term_file_sorted,output_json)
     
     '''
     #for proteins
     term_file='./Glygen/OGER/uniprot_human_sprot_final.csv'
     term_file_sorted='./Glygen/OGER/uniprot_human_sprot_sorted.csv'
-    output_json='./Glygen/OGER/oger_entity.json'
+    output_json='./Glygen/OGER/glygen_full_length_oger_entity.json'
 
     merge_oger_detections(term_file,term_file_sorted,output_json)
    
+    '''
     #for complex-protein
     term_file='./Glygen/OGER/uniprot_human_sprot_cp_final.csv'
     term_file_sorted='./Glygen/OGER/uniprot_human_sprot_cp_sorted.csv'
-    output_json='./Glygen/OGER/oger_entity_cp.json'
+    output_json='./Glygen/OGER/glygen_large_oger_entity_cp.json'
 
     merge_oger_detections(term_file,term_file_sorted,output_json)
-
     '''
+
+    
     
     
